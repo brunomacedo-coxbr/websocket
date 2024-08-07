4e20c2c26abbc964f3f49088462a7be6428948da
Bruno-Santos <bruno.santos@coxautoinc.com>
2024-08-07 13:59:47 -0300

    style: improve layout

--- web-server/src/app/page.tsx
+++ web-server/src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8080');
+    const ws = new WebSocket("ws://localhost:8080");
     setSocket(ws);
 
     return () => {
@@ -17,20 +17,23 @@ const Home: React.FC = () => {
   const sendMessage = () => {
     if (socket) {
       socket.send(message);
-      setMessage('');
+      setMessage("");
     }
   };
 
   return (
-    <div>
-      <h1>WebSocket com Next.js e TypeScript</h1>
-      <input
-        type="text"
-        value={message}
-        className="border"
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Enviar</button>
+    <div className="text-center flex items-center justify-center p-8">
+      <div className="p-4 border max-w-sm w-full">
+        <h1>WebSocket Client-Server</h1>
+        <input
+          type="text"
+          value={message}
+          className="border p-2 m-2 text-base"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Place your message here..."
+        />
+        <button className="py-2 px-6 bg-blue-600 text-white text-base" onClick={sendMessage}>Enviar</button>
+      </div>
     </div>
   );
 };
