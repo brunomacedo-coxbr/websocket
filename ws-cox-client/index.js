90c45eff1c97a9aa085c0ca9ea68a8577d8ea429
Bruno-Santos <bruno.santos@coxautoinc.com>
2024-08-29 17:56:02 -0300

    feat(WebSocketAPI): auto reconnect

--- ws-cox-client/index.js
+++ ws-cox-client/index.js
@@ -1,31 +1,60 @@
 export default function WebSocketClient(url, token, onMessage) {
-  const socket = new WebSocket(url);
+  let socket;
+  let reconnectAttempts = 0;
+  const maxReconnectAttempts = 10;
+  const reconnectInterval = 5000; // 5 seconds
 
-  socket.onopen = () => {
-    socket.send(JSON.stringify({ type: "auth", token: token }));
-  };
+  const connect = () => {
+    socket = new WebSocket(url);
 
-  socket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+    socket.onopen = () => {
+      reconnectAttempts = 0; // Reset reconnect attempts on successful connection
+      socket.send(JSON.stringify({ type: "auth", token: token }));
+    };
 
-    if (data.type === "auth") {
-      if (data.success) {
-        console.log("Authenticated successfully.");
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (data.type === "auth") {
+        if (data.success) {
+          console.log("Authenticated successfully.");
+
+        } else {
+          console.error("Authentication failed.");
+          socket.close();
+        }
 
       } else {
-        console.error("Authentication failed.");
-        socket.close();
+        onMessage(data);
       }
+    };
 
-    } else {
-      onMessage(data);
+    socket.onclose = () => {
+      if (reconnectAttempts < maxReconnectAttempts) {
+        console.log(`Socket closed. Attempting to reconnect... (${ reconnectAttempts + 1 }/${maxReconnectAttempts})`);
+        reconnectAttempts++;
 
-    }
+        setTimeout(connect, reconnectInterval);
+      } else {
+        console.error("Max reconnect attempts reached. Giving up.");
+      }
+    };
+
+    socket.onerror = (error) => {
+      console.error(`Socket encountered error. ${JSON.stringify(error)}`);
+      socket.close();
+    };
   };
+
+  connect();
 
   return {
     sendMessage: (message) => {
-      socket.send(message);
+      if (socket.readyState === WebSocket.OPEN) {
+        socket.send(message);
+      } else {
+        console.error("Socket is not open. Message not sent.");
+      }
     },
     disconnect: () => {
       socket.close();
